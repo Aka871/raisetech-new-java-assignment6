34c9283f4de1e7eb0b3ac9cc7716b309dcea4b4e
Aka871 <163530203+Aka871@users.noreply.github.com>
2024-06-10 22:20:43 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) {
+    List<String> studentNameList = List.of("佐藤", "鈴木", "七五三掛", "田中", "田母神", "小鳥遊", "山口", "伊藤", "田口", "山田");
+  }
+}
