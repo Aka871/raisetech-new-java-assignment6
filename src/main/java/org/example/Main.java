f67fd5d56b5b9daf706dbe767aa48ea8d94bfcaf
Aka871 <163530203+Aka871@users.noreply.github.com>
2024-06-10 23:22:04 +0900

    生徒名が3文字以上なら表示させる、条件分岐式と繰り返し処理を追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,11 @@ public class Main {
 
   public static void main(String[] args) {
     List<String> studentNameList = List.of("佐藤", "鈴木", "七五三掛", "田中", "田母神", "小鳥遊", "山口", "伊藤", "田口", "山田");
+
+    for (String studentName : studentNameList) {
+      if (studentName.length() >= 3) {
+        System.out.println(studentName);
+      }
+    }
   }
 }
